3a7136bade37195b000e3b7ca9cefd4c291f2e73
AniketDubey <aniket.dubey0206@gmail.com>
2025-08-04 21:07:52 +0530

    High Order Function

--- src/Lamda.kt
+++ src/Lamda.kt
@@ -8,10 +8,10 @@ fun main(){
         println(x)
         y + 10
     }*/
-    lambdaFunc(10,aniketLamda)
+    highOrderFunction(10,aniketLamda)
 }
 
-fun lambdaFunc(param1:Int,param2: (String,Int)->Int){
+fun highOrderFunction(param1:Int, param2: (String, Int)->Int){
     println(param1)
 //    println(param2.invoke("Aniket",100))
     val finalAns = param2("Aniket",100)
